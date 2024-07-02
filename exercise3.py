3e9e2dc15ac5c139cdd76dc1a8aca6f346fd5405
f3j2i <fujii.ricky.123@gmail.com>
2024-07-02 16:54:53 +0900

    Modified exercise3.py

--- exercise3.py
+++ exercise3.py
@@ -3,6 +3,9 @@ import random
 def toss_coin():
     return "Heads" if random.randint(0, 1) == 0 else "Tails"
 
+name = input("Who are you?\n> ")
+print(f"Hello, {name}!")
+
 print("Tossing a coin...")
 results = [toss_coin() for _ in range(3)]
 
@@ -15,6 +18,6 @@ tails_count = results.count("Tails")
 print(f"Heads: {heads_count}, Tails: {tails_count}")
 
 if heads_count > tails_count:
-    print("You won!")
+    print(f"{name} won!")
 else:
-    print("You lost!")
+    print(f"{name} lost!")
